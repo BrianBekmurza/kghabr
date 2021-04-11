e168201b42120440ad9a5593f8d6ba957b15735e
Asylbek Bekmurzaev <bekmurzaev.asylbek@gmail.com>
2021-04-12 01:10:49 +0600

    added login test

--- core/factories.py
+++ core/factories.py
@@ -1,9 +1,16 @@
+from django.contrib.auth import get_user_model
 import factory
 from .models import Article
 
-
+User = get_user_model()
 
 class ArticleFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Article
     title = factory.Sequence(lambda n: f"My awsome test {n}")
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+    username = factory.Sequence(lambda n: f"testuser{n}")
+    password = factory.PostGenerationMethodCall('set_password', 'test1234')
--- core/tests.py
+++ core/tests.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
 from django.urls import reverse
 from .models import Article 
-from .factories import ArticleFactory
+from .factories import ArticleFactory, UserFactory
 
 # Create your tests here.
 class HomepageTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('articles')
+
     def test_homepage_loads_success(self):
-        url = reverse('articles')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'article')
 
@@ -23,8 +25,7 @@ class HomepageTestCase(TestCase):
         for i in range(n):
             article = ArticleFactory()
 
-        url = reverse('articles')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertIn('articles', response.context)
         articles = Article.objects.filter(is_active=True)
         self.assertEqual(articles.count(), n)
@@ -33,4 +34,11 @@ class HomepageTestCase(TestCase):
             self.assertContains(response, article.title)
             print(article.title)
             self.assertContains(response, article.text)
+
+class LoginTestCase(TestCase):
+    def test_login_success(self):
+        user = UserFactory()
+        url = reverse('login')
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 200)
 
